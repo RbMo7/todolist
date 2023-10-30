53a373d521592f97083bc68ef49497f8f50bcc34
Mohit Rajbhandari <rbmohit1283@gmail.com>
2023-10-30 21:42:22 +0545

    version 2.1 view formatting

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -67,10 +67,11 @@ const TaskList = ({
         <div>
           <ul className="list-group">
             {items.map((item, num) => {
-              // console.log(num, item);
               return (
                 <li className="list-group-item " key={num}>
-                  <button className="btn" onClick={() => deleteItem(item, num)}>
+                   <div className="row">
+                    <div className="col-auto">
+                  <button className="btn" onClick={() => deleteItem(item, num)} id="pend">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="16"
@@ -81,8 +82,11 @@ const TaskList = ({
                     >
                       <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
                     </svg>
-                  </button>
-                  {item}
+                  </button></div>
+                  <div className="col">
+                      <div className="align-middle"><span className="align-middle">{item}</span></div>
+                    </div>
+                  </div>
                 </li>
               );
             })}
@@ -92,23 +96,29 @@ const TaskList = ({
         <div>
           <ul className="list-group">
             {completedTask.map((item, num) => {
-              // console.log(num, item);
+
               return (
                 <li className="list-group-item " key={num}>
-                  <button className="btn" onClick={() => deleteTask(num)}>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-x"
-                      viewBox="0 0 16 16"
-                    >
-                      {" "}
-                      <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />{" "}
-                    </svg>
-                  </button>
-                  {item}
+                  <div className="row">
+                    <div className="col-auto">
+                      <button className="btn" onClick={() => deleteTask(num)} id="pend">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          className="bi bi-x"
+                          viewBox="0 0 16 16"
+                        >
+                          {" "}
+                          <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />{" "}
+                        </svg>
+                      </button>
+                    </div>
+                    <div className="col">
+                      <div className="align-middle"><span className="align-middle">{item}</span></div>
+                    </div>
+                  </div>
                 </li>
               );
             })}
